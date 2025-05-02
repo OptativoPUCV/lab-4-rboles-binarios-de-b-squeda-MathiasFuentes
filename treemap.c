f99faaa6da685daa2fa802bf23be025b3bd95ae3
Your Name <you@example.com>
2025-05-02 17:08:22 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -96,9 +96,20 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
     tree->current = newNode;                                // Se actualiza el current.
 }
 
-TreeNode * minimum(TreeNode * x){
+/*4.- Implemente la función TreeNode * minimum(TreeNode * x). 
+Esta función retorna el nodo con la mínima clave ubicado en el subárbol con raiz x. 
+Para obtener el nodo tiene que, a partir del nodo x, irse por la rama izquierda hasta llegar al final del subárbol.
+ Si x no tiene hijo izquierdo se retorna el mismo nodo.*/
 
-    return NULL;
+TreeNode * minimum(TreeNode * x){
+    if (x == NULL) return NULL;
+    if (x->left == NULL) return x;
+
+    TreeNode* aux = x;
+    while (aux->left != NULL){
+        aux = aux->left;
+    }
+    return aux;
 }
 
 
